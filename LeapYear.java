b36f8e14fb43297ae24b7a9e5324c925e3c63155
Raisul Islam <45175845+raisulislamg4@users.noreply.github.com>
2022-02-21 03:48:13 +0600

    leap year problem solve

---
+++ LeapYear.java
@@ -0,0 +1,17 @@
+public class LeapYear {
+
+    public static void main(String[] args) {
+
+        int year;
+
+        year = 2016;
+
+        if (year % 4 == 0){
+            System.out.println(year + " is Leap Year!");
+        }
+        else {
+            System.out.println(year + " is not Leap Year!");
+        }
+
+    }
+}
